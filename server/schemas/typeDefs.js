3c38d409393cd797d0cb515b950432d32d77ee90
Lotte-Et <charlotte.stowe@gmail.com>
2024-05-11 14:16:35 -0400

    Continuing to implement back end changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,4 +13,17 @@ const typeDefs = `
         password: String!
         savedBooks: [book]
     }
+
+    type Query {
+        book: [Book]
+        users(username: String): [User]
+    }
+
+    type Mutation {
+        createUser(username: String!, password: String!, email: String!): User
+        getSingleUser(username: String!): User
+        saveBook(bookId: String!): Book
+        deleteBook (username: String!, bookId: string!): User
+        login(username: String!, password: String!): User
+  }
 `
