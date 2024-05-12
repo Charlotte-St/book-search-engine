c1a7bf36f6c8d798679b63892773f724fdace615
Lotte-Et <charlotte.stowe@gmail.com>
2024-05-12 16:10:59 -0400

    Troubleshooting issue with graphQL endpoint for saveBook

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -24,6 +24,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook, {error}] = useMutation(SAVE_BOOK);
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -76,9 +78,9 @@ const SearchBooks = () => {
 
     try {
       //const response = await saveBook(bookToSave, token);
-
-      const [saveBook, {error}] = useMutation(SAVE_BOOK);
-      const response = await saveBook( bookToSave, token);
+      const { data } = await saveBook({
+        variables: { newBook: { ...bookToSave}}
+      });
 
       if (!response.ok) {
         throw new Error(error);
