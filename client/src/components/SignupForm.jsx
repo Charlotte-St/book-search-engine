f8912ec6d0e319204ef44c60dba4136a1f9bd78a
Lotte-Et <charlotte.stowe@gmail.com>
2024-05-14 15:23:20 -0400

    Preparing for Render deployment

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-//import { createUser } from '../utils/API';
-
 import { CREATE_USER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 
